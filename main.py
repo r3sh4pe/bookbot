8452e43288233ded5173b8020c1e59901e4d70a7
r3sh4p3 <reshape.kek@gmail.com>
2025-02-15 15:35:25 +0100

    added word count

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
     book_content: str | None = None
+    words: list[str] | None = None
+    word_count: int | None = None
 
     with open("books/frankenstein.txt", "r") as f:
         book_content = f.read()
 
-    print(book_content)
+    words = book_content.split()
+    word_count = len(words)
+
+    print(word_count)
 
 
 main()
