0ed1c7266c096479d30b94df8ed699ad1e0f5973
r3sh4p3 <reshape.kek@gmail.com>
2025-02-15 15:50:44 +0100

    added char count

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-def main():
+def count_characters(book_content: str) -> dict[str, int]:
+    character_count: dict[str, int] = {}
+
+    for character in book_content:
+        if character in character_count:
+            character_count[character] += 1
+        else:
+            character_count[character] = 1
+
+    return character_count
+
+
+def main() -> None:
     book_content: str | None = None
     words: list[str] | None = None
     word_count: int | None = None
+    char_count: dict[str, int] | None = None
 
     with open("books/frankenstein.txt", "r") as f:
         book_content = f.read()
@@ -9,7 +22,9 @@ def main():
     words = book_content.split()
     word_count = len(words)
 
-    print(word_count)
+    char_count = count_characters(book_content.lower())
+
+    print(char_count)
 
 
 main()
