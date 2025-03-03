b8675d6922dd5b440cf49b55d4512822e803c217
r3sh4p3 <reshape.kek@gmail.com>
2025-03-03 01:48:20 +0100

    ch2 l6

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-def create_report(word_count: int, char_count: dict[str, int]) -> None:
-    report_string: str = f"--- Begin report of books/frankenstein.txt ---\n{word_count} words found in the document\n\n"
-    sorted_char_count = dict(sorted(char_count.items(), key=lambda c: c[1], reverse=True))
-
-    for k, v in sorted_char_count.items():
-        if k in "abcdefghijklmnopqrstuvwxyz":
-            report_string += f"The '{k}' character was found {v} times\n"
-
-    report_string += "--- End report ---"
-    print(report_string)
-
-
-def count_characters(book_content: str) -> dict[str, int]:
-    character_count: dict[str, int] = {}
-
-    for character in book_content:
-        if character in character_count:
-            character_count[character] += 1
-        else:
-            character_count[character] = 1
-
-    return character_count
+from stats import get_num_words, count_characters, create_report
 
 
 def main() -> None:
@@ -31,8 +10,7 @@ def main() -> None:
     with open("books/frankenstein.txt", "r") as f:
         book_content = f.read()
 
-    words = book_content.split()
-    word_count = len(words)
+    word_count = get_num_words(book_content)
 
     char_count = count_characters(book_content.lower())
 
